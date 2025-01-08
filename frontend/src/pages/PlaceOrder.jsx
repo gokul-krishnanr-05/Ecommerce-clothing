a9e64c40ca336c487b42c54b0ab6269d9214354d
krishnendhu-ps <krishnendhups91@gmail.com>
2025-01-08 22:52:34 +0530

    Added Place order in pages folder

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -128,7 +128,6 @@ const PlaceOrder = () => {
 
     return (
         <form onSubmit={onSubmitHandler} className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
-            {/* ------------- Left Side ---------------- */}
             <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
 
                 <div className='text-xl sm:text-2xl my-3'>
@@ -151,7 +150,6 @@ const PlaceOrder = () => {
                 <input required onChange={onChangeHandler} name='phone' value={formData.phone} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Phone' />
             </div>
 
-            {/* ------------- Right Side ------------------ */}
             <div className='mt-8'>
 
                 <div className='mt-8 min-w-80'>
@@ -160,7 +158,6 @@ const PlaceOrder = () => {
 
                 <div className='mt-12'>
                     <Title text1={'PAYMENT'} text2={'METHOD'} />
-                    {/* --------------- Payment Method Selection ------------- */}
                     <div className='flex gap-3 flex-col lg:flex-row'>
                         <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                             <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
